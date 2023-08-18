ac86564edf5a82230ab4d374dd480ae9acefcb1c
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2023-08-18 18:29:51 +0200

    Fixed item context menu.

--- src/components/item/ItemContextMenu.tsx
+++ src/components/item/ItemContextMenu.tsx
@@ -2,10 +2,10 @@ import * as ContextMenu from "@radix-ui/react-context-menu";
 
 export const ItemContextMenu = ()=>{
     return <>
-        <ContextMenu.Item className="border-b-[1px] border-white p-1 cursor-pointer">Open in Tab</ContextMenu.Item>
-        <ContextMenu.Item className="border-b-[1px] border-white p-1 cursor-pointer">Rename</ContextMenu.Item>
-        <ContextMenu.Item className="border-b-[1px] border-white p-1 cursor-pointer">Duplicate</ContextMenu.Item>
-        <ContextMenu.Item className="border-b-[1px] border-white p-1 cursor-pointer">Delete</ContextMenu.Item>
+        <ContextMenu.Item className=" p-1 cursor-pointer">Open in Tab</ContextMenu.Item>
+        <ContextMenu.Item className=" p-1 cursor-pointer">Rename</ContextMenu.Item>
+        <ContextMenu.Item className=" p-1 cursor-pointer">Duplicate</ContextMenu.Item>
+        <ContextMenu.Item className=" p-1 cursor-pointer">Delete</ContextMenu.Item>
 
     </>
 }
